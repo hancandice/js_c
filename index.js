12c4d7c33152346e308668ce19162cc900409147
Candice Han <jeeyoung.han82@gmail.com>
2020-07-03 00:23:00 +0200

    ADD clock and greeting features

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("I'm working on JS. I'm pretty. And I deserve the best!");
+// DOM(Document Object Module)
+
+const title = document.querySelector("#title");
+title.innerHTML = "Hi ★ From JS";
+title.style.color = "red";
+document.title = "I own you now";
+
+title.addEventListener("click", handleClick);
+
+function handleClick() {
+  title.style.color = "blue";
+}

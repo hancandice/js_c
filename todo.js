d4d9df9297b19825277a8deeba76e9ff16974d22
Candice Han <jeeyoung.han82@gmail.com>
2020-07-03 01:21:44 +0200

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "🚫";
   const span = document.createElement("span");
+  const newID = toDos.length + 1;
+  delBtn.innerText = "🚫";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newID;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newID,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +37,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 

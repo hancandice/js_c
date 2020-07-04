47ac50bce4613b7f5b9183e9034972b730b257c0
Candice Han <jeeyoung.han82@gmail.com>
2020-07-04 10:53:29 +0200

    ADD pengsoo-images

--- bg.js
+++ bg.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 11;
+const IMG_NUMBER = 9;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `/images/${imgNumber}.jpg`;
+  image.src = `/pengimages/${imgNumber}.jpg`;
   image.classList.add("bgImage");
   body.appendChild(image);
 }
